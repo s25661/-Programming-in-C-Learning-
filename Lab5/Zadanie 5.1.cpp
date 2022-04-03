6e142f1072dda6d5ab221b2c05fc0b093ed882e5
Damian Leszczyński <92215052+s25661@users.noreply.github.com>
2022-04-03 19:16:24 +0200

    Add files via upload

---
+++ Lab5/Zadanie 5.1.cpp
@@ -0,0 +1,46 @@
+﻿//Napisz program, w którym: 
+//a) użytkownik poda liczbę elementów wektora;
+//b) pobrane zostaną od użytkownika wszystkie elementy wektora;
+//c) wskazana zostanie wartość elementu maksymalnego wektora;
+//d) Wskazany zostanie numer indeksu elementu maksymalnego.
+#include <iostream>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+
+auto main() -> int
+{
+	int liczba_elementow, elementy,max;
+
+	cout << "Podaj liczbe elementow wektora: ";
+	cin >> liczba_elementow;
+	cout << endl; 
+	cout << "Podaj wartosci elementow wektora.\n" << endl;
+		
+	vector<int>tab(liczba_elementow);
+		
+	for (int i = 0; i < liczba_elementow; i++) {
+
+		cout << "Podaj " << i + 1 << " wartosc: "; 
+		cin >> tab[i];
+	}
+
+	int max1 = 0, maxIndex = 0, i = 0;
+
+	for (auto const& element : tab)
+	{
+		if (element > max1)
+		{
+			max1 = element;
+			maxIndex = i;
+		}
+		i++;
+	}
+	cout << endl;
+	cout << "Najwiekszy element to " << max1 << " a jego index wynosi " << maxIndex + 1;
+	cout << endl;
+	return 0; 
+}
---
+++ Lab5/Zadanie 5.2.cpp
@@ -0,0 +1,70 @@
+﻿//Napisz program, który umożliwia:
+//a) wczytanie aktualnego rozmiaru tablicy liczb całkowitych;
+//b) wczytanie elementów tablicy liczb całkowitych;
+//c) zamianę miejscami elementów tablicy, lezących po przeciwnej stronie prostej pionowej, dzielącej tablicę na dwie równe części.
+#include <iostream>
+#include <array>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::swap;
+
+auto main() -> int
+{
+	int elementy, wartosci;
+
+	cout << "Podaj liczbe elementow tablicy: ";
+	cin >> elementy;
+	cout << endl;
+
+	int* tab;
+	tab = new int[elementy];
+
+	for (int i = 0; i < elementy; i++) {
+
+		cout << "Podaj " << i + 1 << " element: ";
+		cin >> wartosci;
+		tab[i] = wartosci;
+
+	}
+
+	cout << endl << "Przed odwroceniem:\n" << endl;
+
+	for (int i = 0; i < elementy; i++) {
+
+		if (i < elementy / 2) {
+			cout << tab[i] << '\t';
+
+		}
+		else {
+			cout << '\t';
+			cout << tab[i];
+
+		}
+		cout << endl;
+	}
+
+	for (int i = 0; i < elementy / 2; i++) {
+
+		swap(tab[i], tab[elementy - 1 - i]);
+
+	}
+
+	cout << endl << "Po odwroceniu:\n" << endl;
+
+	for (int i = 0; i < elementy; i++) {
+
+		if (i < elementy / 2) {
+			cout << tab[i] << '\t';
+
+		}
+		else {
+			cout << '\t';
+			cout << tab[i];
+
+		}
+		cout << endl;
+	}
+	return 0;
+}
